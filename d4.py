c3ef6986e55c3616d49925ebc595703a58e12098
burningshed <burningshed@gmail.com>
2024-12-04 23:27:59 -0500

    d4 code in the car

--- d4.py
+++ d4.py
@@ -11,8 +11,8 @@ Day = 4
 Year = 2024
 ReadyA = False
 ReadyB = False
-TestA = True
-TestB = False
+TestA = False
+TestB = True
 
 # get puzzle 
 token = open("./.config/aocd/token", 'r').read()
@@ -33,13 +33,11 @@ def part1(data):
         return substring.count("xmas")
     def get_xmases(substring_arr):
         cur_tot = 0
-        for line in substring_arr:
-            cur_tot += get_xmas(line)
-        for line in substring_arr:
-            cur_tot += get_xmas(line[::-1])
+        cur_tot += get_xmas(substring_arr)
+        cur_tot += get_xmas(substring_arr[::-1])
         return cur_tot
     def unnumpy(numpy_arr):
-        new_arr = np.array2string(numpy_arr, separator='').replace('\'','').replace('[','').replace(']','')
+        new_arr = np.array2string(numpy_arr, separator='').replace('\'','').replace('[','').replace(']','').replace('\n','').replace(" ","")
         print(new_arr)
         return new_arr
     total = 0
@@ -63,6 +61,7 @@ def part1(data):
         return cur_tot
     
     total += get_diag_xmas(chr_arr_np)
+    print()
     total += get_diag_xmas(np.fliplr(chr_arr_np))
     #total -= get_xmases(unnumpy(chr_arr_np.diagonal()))
     return str(total)
@@ -70,6 +69,50 @@ def part1(data):
         
 # part 2
 def part2(data):
+    data = data.lower()
+    str_arr = data.splitlines()
+    chr_arr = []
+    for line in data.splitlines():
+        chr_arr.append([c for c in line])
+    str_arr_np = np.array(str_arr)
+    chr_arr_np = np.array(chr_arr)
+    def unnumpy(numpy_arr):
+        new_arr = np.array2string(numpy_arr, separator='').replace('\'','').replace('[','').replace(']','').replace('\n','').replace(" ","")
+        print(new_arr)
+        return new_arr
+    def check_corner(arr, index, cor):
+        print(index, cor)
+        cur_index = index + cor
+        print(index, cor, cur_index)
+        opp_index = index + np.array(cor) * -1
+        print(opp_index, index, cor, cur_index)
+        if arr[cur_index[0],cur_index[1]] == 'm' and arr[opp_index[0], opp_index[1]] == 's':
+            return True
+        if arr[cur_index[0],cur_index[1]] == 's' and arr[opp_index[0], opp_index[1]] == 'm':
+            return True
+        return False
+    def check_corners(arr, index):
+        main_corners = [(-1,-1), (-1,1)]
+        for corner in main_corners:
+            if check_corner(arr, index, corner):
+                continue
+            else:
+                return False
+        return True
+                
+                
+        print(arr[index[0], index[1]])
+    total = 0
+    
+    print(chr_arr_np)
+    active_chr_arr_np = chr_arr_np[1:-1,1:-1]
+    print(active_chr_arr_np)
+    a_list = np.argwhere(active_chr_arr_np=='a')+1
+    for element in a_list:
+        if check_corners(chr_arr_np, element):
+            total += 1
+    print()
+    return str(total)
     pass
 
 custom_example = [
@@ -85,7 +128,7 @@ S.S.S.S.SS
 ..M.M.M.MM
 .X.X.XMASX""", 
         '18', 
-        None
+        '9'
         ),
     Example(
         """MMMSXXMASM
@@ -98,17 +141,27 @@ SMSMSASXSS
 SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX""",
-        None,
-        None
-        )
+        '18',
+        '9'
+        ),
+    Example(
+    """MXMAS
+xMMMM
+MmAMX
+AsAmx
+sXXsX""",
+    '4',
+    None
+    )
     ]
 
 if TestA:
     tester(part1, 'a', puzzle, custom_example)
     
 elif TestB:
-    tester(part2, 'b', puzzle)
+    tester(part2, 'b', puzzle, custom_example)
             
+print(part2(puz_data))
             
             
 # solution and submit
